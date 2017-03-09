5f8f9920a922243c1c2b2baf8d65c26dd7fc339e
kyveney <ky.veney63@gmail.com>
2017-03-09 12:07:30 -0600

    added index.html, some other minor changes

--- service/volunteers/views.py
+++ service/volunteers/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Volunteer
 # Create your views here.
+def index(request):
+    all_volunteers = Volunteer.objects.all()
+    return render(request, 'volunteers/index.html', {'all_volunteers': all_volunteers})
