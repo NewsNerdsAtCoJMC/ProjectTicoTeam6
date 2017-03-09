2a552aadb0609ba924a9c60ac1af55e2ce45474d
bschulz14 <schulz.bailey@gmail.com>
2017-03-09 11:40:24 -0600

    added details to models, created model for organizations

--- service/organizations/models.py
+++ service/organizations/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField()
+    def __str__(self):
+        return self.name
--- service/volunteers/models.py
+++ service/volunteers/models.py
@@ -4,5 +4,8 @@ class Volunteer(models.Model):
     name = models.CharField(max_length=255)
     dob = models.DateField()
     email_address = models.EmailField()
+    picture = models.ImageField()
+    GENDER_CHOICES = ((FEMALE, 'Female'),(MALE, 'Male'),)
+    gender = models.CharField(max_length=2,choices=GENDER_CHOICES,default=MALE,)
     def __str__(self):
         return self.name
