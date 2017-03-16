ae1f52168e19254593fda74b8da2d44e647eba68
bschulz14 <schulz.bailey@gmail.com>
2017-03-16 12:02:18 -0500

    fixing errors -- URLs, templates

--- service/volunteers/views.py
+++ service/volunteers/views.py
@@ -6,6 +6,6 @@ def index(request):
     all_volunteers = Volunteer.objects.all()
     return render(request, 'volunteers/index.html', {'all_volunteers': all_volunteers})
 
-def home(request):
-    hello = "Hello World"
-    return render(request, 'home.html', {'all_volunteers': all_volunteers})
+#def home(request):
+#    hello = "Hello World"
+#    return render(request, 'home.html', {'all_volunteers': all_volunteers})
