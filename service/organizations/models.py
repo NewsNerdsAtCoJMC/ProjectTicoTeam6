f943115628ec04f889c4af7dd762971b0e163ea5
bschulz14 <schulz.bailey@gmail.com>
2017-03-16 12:11:09 -0500

    add "description" to organization admin

--- service/organizations/models.py
+++ service/organizations/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
+    description = models.CharField(max_length=225)
     def __str__(self):
         return self.name
