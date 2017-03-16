ddbd221a91543e3f1debf2fcf96b2e08cf016d2c
bschulz14 <schulz.bailey@gmail.com>
2017-03-16 11:40:00 -0500

    add admin to URLs

--- service/volunteers/urls.py
+++ service/volunteers/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from volunteers import views
 
 urlpatterns = [
     url(r'^$', views.index, name= 'index'),
+    url(r'^admin/', admin.site.urls),
 ]
