5c99ab8e10c40a1273993c1694f52cf4af350397
bschulz14 <schulz.bailey@gmail.com>
2017-03-26 19:17:06 -0500

    Adding organizations to admin, templates to organization

--- service/organizations/models.py
+++ service/organizations/models.py
@@ -3,6 +3,5 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=255)
     location = models.CharField(max_length=255)
-    description = models.CharField(max_length=225)
     def __str__(self):
         return self.name
