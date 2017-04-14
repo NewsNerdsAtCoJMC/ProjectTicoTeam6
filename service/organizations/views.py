4f70d4d9ff94ab418f70fde65cf4cd6d132d3527
Ky Veney <ky.veney63@gmail.com>
2017-04-14 16:46:51 -0500

    updating organizations models, views, templates

--- service/organizations/views.py
+++ service/organizations/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from organizations.models import Organization
 
 def detail(request):
-    all_organizations = Organization.objects.all()
+    all_organizations = Organization.objects.filter(active=True).order_by('entered')
     context = {'all_organizations': all_organizations}
     return render(request, 'organizations/index.html', context)
