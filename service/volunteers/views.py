654184b56b5f8021d41c6f24faf99ab3c4684043
Ky Veney <ky.veney63@gmail.com>
2017-04-25 11:11:30 -0500

    minor template fixes

--- service/volunteers/views.py
+++ service/volunteers/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 
 def index(request):
     context = {}
-    return render(request, 'volunteers/index.html', context)
+    return render(request, '/volunteers/static/index.html/', context)
 
 @login_required(login_url='/accounts/login/')
 def detail(request):
@@ -24,4 +24,4 @@ def detail(request):
         form = VolunteerForm()
         hours = VolunteerHours.objects.filter(user=request.user)
         context = {'form': form, 'hours': hours}
-        return render(request, 'volunteers/detail.html', context)
+        return render(request, '/volunteers/static/volunteers/detail.html', context)
